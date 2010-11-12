fc754f283e748062865a4bb86f3bfda957d4018e
Ondrej Mirtes <ondrej@mirtes.cz>
2010-11-12 18:41:37 +0100

    Person refactored to inherit from SimpleEntity.

--- gowalla-ejb/src/java/cz/cvut/x33eja/gowalla/model/Person.java
+++ gowalla-ejb/src/java/cz/cvut/x33eja/gowalla/model/Person.java
@@ -1,38 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package cz.cvut.x33eja.gowalla.model;
 
-import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
  *
  * @author ondrej
  */
 @Entity
-public class Person implements Serializable {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class Person extends SimpleEntity {
 
     @Column
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -40,31 +19,6 @@ public class Person implements Serializable {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Person)) {
-            return false;
-        }
-        Person other = (Person) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "model.Person[id=" + id + "]";
     }
 
 }
---
+++ gowalla-ejb/src/java/cz/cvut/x33eja/gowalla/model/SimpleEntity.java
@@ -0,0 +1,54 @@
+package cz.cvut.x33eja.gowalla.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author ondrej
+ */
+@MappedSuperclass
+abstract public class SimpleEntity implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof SimpleEntity)) {
+            return false;
+        }
+        SimpleEntity other = (SimpleEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cz.cvut.x33eja.gowalla.model.SimpleEntity[id=" + id + "]";
+    }
+
+}
